e3a5a6d5448087eeca8431fccc02095a9da59900
Akram Fawzi Mihoubi <mihhakram@gmail.com>
2024-07-16 21:58:41 +0100

    add SPA Mode to AdminPanel

--- dashboard-app/app/Providers/Filament/AdminPanelProvider.php
+++ dashboard-app/app/Providers/Filament/AdminPanelProvider.php
@@ -27,6 +27,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->spa()
             ->colors([
                 'primary' => Color::Amber,
             ])
